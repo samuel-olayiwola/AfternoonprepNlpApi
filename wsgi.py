280766081ae638e8ee131efb957e583ec1cc043f
Samuel Peter <you@example.com>
2022-04-11 20:54:21 +0000

    first commit

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ import uvicorn
 
 if __name__ == "__main__":
   if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8050)
+    uvicorn.run(app, host='https://afternoonpepnlp.herokuapp.com')
